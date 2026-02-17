7fa3a100260961834ff3b9b9653cd604d171557d
MRstiven232004 <stiven20042316@gmail.com>
2026-02-17 08:01:30 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Layout />}> 
           {/* Inicio */}
           <Route index element={<Home />} />
 
